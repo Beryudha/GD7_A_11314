c4265094f59f9921d1a7c9dc1c06f5e9b35acbe6
Beryudha <yudhaanggodho@gmail.com>
2023-10-18 15:40:45 +0700

    modified:   app/Http/Controllers/MerchandiseController.php 	modified:   resources/views/merchandise.blade.php 	modified:   routes/web.php

--- routes/web.php
+++ routes/web.php
@@ -17,34 +17,6 @@ Route::get('/', function () {
     return view('landingPage');
 });
 
-// Route::get('/merchandise', function () {
-//     return view('merchandise', [
-//         'barang' => [
-//             [
-//                 'gambar' => 'images/3-o.jpg',
-//             ],
-//             [
-//                 'gambar' => 'images/16.png',
-//             ],
-//             [
-//                 'gambar' => 'images/20.png',
-//             ],
-//             [
-//                 'gambar' => 'images/38-o.jpg',
-//             ],
-//             [
-//                 'gambar' => 'images/46-o.jpg',
-//             ],
-//             [
-//                 'gambar' => 'images/cat.jpg',
-//             ],
-//             [
-//                 'gambar' => 'images/Sup-cuh.jpg',
-//             ]
-//         ]
-//     ]);
-// });
-
 Route::resource('/ticket', App\Http\Controllers\TicketController::class);
 
 Route::resource('/merchandise', App\Http\Controllers\MerchandiseController::class);
